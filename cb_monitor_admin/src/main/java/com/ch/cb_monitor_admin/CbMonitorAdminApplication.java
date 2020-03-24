13ed736192c12c91346d2474359e57c1cb56fd3e
liuchenhao <liuchenhao@hulian120.com>
2020-03-24 22:14:48 +0800

    修改eureka客户端

--- cb_monitor_admin/src/main/java/com/ch/cb_monitor_admin/CbMonitorAdminApplication.java
+++ cb_monitor_admin/src/main/java/com/ch/cb_monitor_admin/CbMonitorAdminApplication.java
@@ -4,11 +4,14 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
 @EnableAdminServer
+@EnableEurekaClient
+@SpringBootApplication
+@EnableDiscoveryClient
 public class CbMonitorAdminApplication {
 
 	public static void main(String[] args) {
