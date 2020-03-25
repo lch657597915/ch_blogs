07a87039c1f62fa36a0402be419b18137fd0e540
liuchenhao <liuchenhao@hulian120.com>
2020-03-25 15:40:35 +0800

    no message

--- cb_eureka/src/main/java/com/ch/cb_eureka/CbEurekaApplication.java
+++ cb_eureka/src/main/java/com/ch/cb_eureka/CbEurekaApplication.java
@@ -3,12 +3,20 @@ package com.ch.cb_eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableEurekaServer
+
 public class CbEurekaApplication {
 
 	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(CbEurekaApplication.class, args);
+
+		String[] activeProfiles = context.getEnvironment().getActiveProfiles();
+		for (String activeProfile : activeProfiles) {
+//			log.info("gt-base 使用环境为: " + activeProfile);
+		}
 		SpringApplication.run(CbEurekaApplication.class, args);
 	}
 
