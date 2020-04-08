47967d9045057fea4bcbdf34a2116f09b93b6f13
liuchenhao <liuchenhao@hulian120.com>
2020-04-09 00:14:54 +0800

    初始化common和user

---
+++ cb_user/src/main/java/com/ch/cb_user/CbUserApplication.java
@@ -0,0 +1,14 @@
+package com.ch.cb_user;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbUserApplication.class, args);
+	}
+
+}
