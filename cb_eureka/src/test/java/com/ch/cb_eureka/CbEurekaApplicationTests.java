133e8f7d84bef6aeb5de6bb077df93ceb57e688d
liuchenhao <liuchenhao@hulian120.com>
2020-04-12 23:27:04 +0800

    no message

--- cb_eureka/src/test/java/com/ch/cb_eureka/CbEurekaApplicationTests.java
+++ cb_eureka/src/test/java/com/ch/cb_eureka/CbEurekaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CbEurekaApplicationTests {
+public class CbEurekaApplicationTests {
 
 	@Test
 	void contextLoads() {
